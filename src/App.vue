0751df3abe52cad44688120fc19e7ebcb6750e58
osamamhd <osamamhd82@gmail.com>
2021-06-27 19:34:36 +0200

    ✨ fixed nav.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,62 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="nav" class="bg-gray-800 fixed w-full flex justify-around py-2 font-serif">
+    <div>
+      <router-link to="/">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mx-auto" viewBox="0 0 20 20" fill="currentColor">
+          <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+        </svg>      
+        <span class="text-xs">
+          Home
+        </span> 
+      </router-link> 
+    </div>
+
+    <div>
+      <router-link to="/dreams">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
+        </svg>
+        <span class="text-xs">
+          Dreams
+        </span>
+      </router-link> 
+    </div>
+
+    <div class="bg-gray-400 rounded-full">
+      <router-link to="/create">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 my-auto" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd" />
+        </svg>
+      </router-link> 
+    </div>
+
+    <div>
+      <router-link to="/nightmares">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mx-auto" viewBox="0 0 20 20" fill="currentColor">
+          <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z" />
+        </svg>
+        <span class="text-xs">
+          Nightmates
+        </span>
+      </router-link> 
+    </div>
+    
+    <div>
+      <router-link to="/about">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mx-auto" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd" />
+        </svg>
+        <span class="text-xs">
+          About
+        </span>
+      </router-link> 
+    </div>
+
   </div>
-  <router-view/>
+
+  <div class="pt-20">
+    <router-view/>
+  </div>
 </template>
 
 <style>
@@ -15,16 +68,13 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  color: #54616e;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  font-weight: bold;
+  color: #fffeee;
 }
 </style>
