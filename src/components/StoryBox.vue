e7a6a530d08e5efd09579ba6fd72a9ba7f2b3054
osamamhd <osamamhd82@gmail.com>
2021-06-28 17:10:01 +0200

    ⚡️ Create StoryBox component.

---
+++ src/components/StoryBox.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    {{ story.title }}
+    {{ story.pub_data }}
+    {{ story.body }}
+
+</div>
+
+</template>
+
+<script>
+export default {
+    name: 'StoryBox',
+
+    props: {
+        story: Object
+    }
+
+}
+</script>
+
+<style>
+
+</style>
