423c18178b0d4d20d553c8074bcadda972318d37
osamamhd <osamamhd82@gmail.com>
2021-06-28 17:09:05 +0200

    ✨ font style and resize nightmares icon.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="nav" class="bg-gray-800 fixed w-full flex justify-around py-2 font-serif">
+  <div id="nav" class="bg-gray-800 fixed w-full flex justify-around py-2">
     <div>
       <router-link to="/">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mx-auto" viewBox="0 0 20 20" fill="currentColor">
@@ -32,7 +32,7 @@
 
     <div>
       <router-link to="/nightmares">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mx-auto" viewBox="0 0 20 20" fill="currentColor">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mx-auto" viewBox="0 0 20 20" fill="currentColor">
           <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z" />
         </svg>
         <span class="text-xs">
@@ -61,7 +61,7 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
