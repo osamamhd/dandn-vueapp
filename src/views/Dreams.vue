6bc22cbbe1967089d9cd8389d263ddd43dd35e37
osamamhd <osamamhd82@gmail.com>
2021-06-28 17:11:24 +0200

    ✨ data, methods for dreams stories

--- src/views/Dreams.vue
+++ src/views/Dreams.vue
@@ -1,10 +1,49 @@
 <template>
-  <h1>Dreams</h1>
+
+  <story-box
+    v-for="story in dreamsStories"
+    :key="story.id"
+    :story="story" />
+  
 </template>
 
 <script>
-export default {
+import axios from 'axios'
+import StoryBox from '@/components/StoryBox'
 
+export default {
+  name: 'Dreams', 
+
+  data() {
+    return {
+      dreamsStories: []
+    }
+  }, 
+
+  components: {
+    StoryBox
+  },
+
+  mounted() {
+    this.getDreamsStories()
+
+    document.title = 'D&&N | Dreams'
+  }, 
+
+  methods: {
+    async getDreamsStories() {
+
+      await axios
+        .get('/stories/api/list/dreams/')
+        .then(response => {
+          this.dreamsStories = response.data
+        })
+
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  }
 }
 </script>
 
