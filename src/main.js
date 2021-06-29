939446bf80d7cd6bcb7eb280219c9086d55c5e46
osamamhd <osamamhd82@gmail.com>
2021-06-29 20:34:33 +0200

    🔖 change baseURL.

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import router from './router'
 import '../public/styles.css'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000/'
+axios.defaults.baseURL = 'http://dreamsandnightmares.pythonanywhere.com/'
 
 
 createApp(App).use(router).mount('#app')
